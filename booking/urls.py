e5d99e70246eae967bd5d567c31dadfc2f67d414
Jaap Joris Vens <jj@rtts.eu>
2019-04-18 23:13:10 +0200

    Automated commit for deployment

--- booking/urls.py
+++ booking/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
+from django.shortcuts import redirect
 from .views import Welcome, Schedule
 
 app_name = 'booking'
 
+def homepage(request):
+    return redirect('http://www.superformosa.nl/')
+
 urlpatterns = [
+    path('', homepage),
     path('<slug:slug>/', Welcome.as_view(), name='welcome'),
     path('<slug:slug>/add/', Schedule.as_view(), name='schedule'),
 ]
